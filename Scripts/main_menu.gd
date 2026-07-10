00fc34f3ddd8b55a7eab063ca812f2332f6d0732
wol-existss <wol.chan@icloud.com>
2026-07-10 11:30:58 -0400

    Changed button offsets

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,14 +12,17 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/misc/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/game/level1.tscn")
 
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+	
+	
 
 
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/misc/settings.tscn") # Replace with function body.
+	
